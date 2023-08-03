2bb7d7692397376099d068664d4567255d5cd510
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-03 17:02:54 -0300

    Update agregar_dispositivo_bluetooth.bat

--- agregar_dispositivo_bluetooth.bat
+++ agregar_dispositivo_bluetooth.bat
@@ -1,4 +1,6 @@
 @echo off
+title - https://github.com/OtaconEvil
+
 REM -- Abre la ventana de Agregar dispositivo Bluetooth --
 echo Abriendo la ventana de Agregar dispositivo Bluetooth...
 control /name Microsoft.DevicesAndPrinters
