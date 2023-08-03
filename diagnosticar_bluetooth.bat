8b5d7cda445c028c63608718a9be6407a1b131b4
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-03 17:03:16 -0300

    Update diagnosticar_bluetooth.bat

--- diagnosticar_bluetooth.bat
+++ diagnosticar_bluetooth.bat
@@ -1,4 +1,6 @@
 @echo off
+title - https://github.com/OtaconEvil
+
 REM -- Abre el solucionador de problemas de Bluetooth --
 echo Ejecutando el solucionador de problemas de Bluetooth...
 msdt.exe -id BluetoothDiagnostic
